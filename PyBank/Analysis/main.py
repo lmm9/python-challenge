e1f87933e217f116a1770b2b2042b29dca6d7198
Laura <laura9michael@gmail.com>
2020-12-29 22:24:02 -0500

    PyBank Solution

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -1,71 +1,83 @@
+#PyBank Homework
+
 # Import Dependencies
 import os
 import csv
-import datetime
 
 #Define the source data
 budget_csv=os.path.join("..","Resources","budget_data.csv")
 
+
+# Track various financial parameters
+total_months = 0
+month_change = []
+net_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 999999999999999999]
+total_net = 0
+
 #Read the csv file
-with open (budget_csv, "r") as csvfile:
+with open (budget_csv, 'r') as csvreader:
     #split the file at commas
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvreader, delimiter=',')
     #Skip the header line
     header = next(csvreader)
-    #test to ensure data present
-    print (header)
     
-    #Convert date column to time format and profit to integer
-    date=datetime.datetime(budget_data[0], "%b-%Y")
-    profit=int(budget_data[1])
-         
-    # Calculate First and Last Values
-    open_date=0
-    close_date=0
-    for i in range(0,len(profit)): 
-        if open_date>date[i]:
-            open_date=date[i]
-            open_profit=profit[i]
-            if close_datet<date[i]:
-                close_date=date[i]
-                close_profit=profit[i]
-
+    #Read the first row to calculate net change
+    first_row = next(csvreader)
+    total_months +=1
+    total_net += int(first_row[1])
+    previous_net = int(first_row[1])
     
-    # Calculate Max and Min profit and dates
-    max_profit ==0   
-    min_profit==0    
-    for i in range(0,len(profit),1): 
-        if max_profit<profit[i]:
-            max_profit=profit[i]
-            max_date=date[i]
-            if min_profit>profit[i]:
-                min_profit=profit[i]
-                min_date=date[i]
+    for row in csvreader:
 
-     total_months=close_date-open_date
-     total_profit=close_profit-open_profit
+        # Track the total
+        total_months += 1
+        total_net += int(row[1])
+
+        # Track the net change
+        net_change = int(row[1]) - previous_net
+        previous_net = int(row[1])
+        net_change_list += [net_change]
+        month_change += [row[0]]
+
+        # Calculate the greatest increase
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+
+        # Calculate the greatest decrease
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+
+# Calculate the Average Net Change
+net_month_avg = sum(net_change_list) / len(net_change_list)
+
+# Create Financial Analysis Summary
+output = (
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_net}\n"
+    f"Average  Change: ${net_month_avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+# Print the output (to terminal)
+print(output)
+
 
 #Define Output path
 output_path = os.path.join("..", "Analysis", "FinancialAnalysis.csv")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w') as csvfile:
+with open(output_path, 'w') as output_file:
 
     # Initialize csv.writer
-    csvwriter = csv.writer(csvfile, delimiter=',')
-
-    # Write the Summary Headers
-    csvwriter.writerow(["'''text"])
-    csvwriter.writerow(["Financial Analysis"])
-    csvwriter.writerow(["-----------------"])
+    csvwriter = csv.writer(output_file, delimiter=',')
     
-
-    # Write the Summary Info
-    csvwriter.writerow(["Total Months: "+total_months])
-    csvwriter.writerow(["Total: "+total_profit])
-    csvwriter.writerow(["Average Change: "+avg_profit])
-    csvwriter.writerow(["Greatest Increase in Profits: "+str(max_date)+ " ("+str(max_profit)+")"])
-    csvwriter.writerow(["Greatest Decrease in Profits: "+str(min_date)+ " ("+str(min_profit)+")"])
+    # Write the output
+    csvwriter.writerow([output])
     
-
-
+  
