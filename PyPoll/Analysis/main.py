c0c08133d43faae2db50bc6856736cb44939b2bc
Laura <laura9michael@gmail.com>
2020-12-29 23:11:40 -0500

    PyPoll Solution

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -1,5 +1,107 @@
-<<<<<<< HEAD
-# Import Dependenciesimport osimport csv#Define the source dataelection_csv=os.path.join("..","Resources","election_data.csv")#Read the csv filewith open (election_csv, "r") as csvfile:    #split the file at commas    csvreader = csv.reader(csvfile, delimiter=',')    #Skip the header line    header = next(csvreader)    #test to ensure data present    print (header)        for row in csvreader:        print (row)        # #Convert Voter ID to integer    # voter_id=int(election_data[0])             # # Calculate Votes    # total_votes =0     # cand_1="Correy"    # cand_2="Khan"    # cand_3="Li"    # cand_4="O'Tooley"    # cand1_votes=0    # cand2_votes=0    # cand3_votes=0    # cand4_votes=0    # for i in range(0,len(voter_id)):     #     total_votes=total_votes+1    #     if cand1==candidate[i]:    #         cand1_votes=cand1_votes +1      #     elif cand2==candidate[i]:    #         cand2_votes=cand2_votes +1    #     elif cand3==candidate[i]:    #         cand2_votes=cand3_votes +1    #     elif cand3==candidate[i]:    #         cand3_votes=cand3_votes +1    #     elif cand4==candidate[i]:    #         cand4_votes=cand4_votes +1                     # #Calculate Percentages    # cand1_percent=cand1_votes/total_votes    # cand2_percent=cand2_votes/total_votes    # cand3_percent=cand3_votes/total_votes    # cand4_percent=cand4_votes/total_votes        # if cand1_percent>cand2_percent and (cand1_percent>cand3_percent and cand1_percent>cand4_percent    #     winner=cand_1    # elif cand2_percent>cand1_percent and cand2_percent>cand3_percent and cand2_percent>cand4_percent    #     winner=cand_2    # elif cand3_percent>cand1_percent and cand3_percent>cand2_percent and cand3_percent>cand4_percent    #     winner=cand_3    # elif cand4_percent>cand1_percent and cand4_percent>cand2_percent and cand4_percent>cand3_percent    #     winner=cand_4 # #Define Output path# output_path = os.path.join("..", "Analysis", "ElectionAnalysis.csv")# # Open the file using "write" mode. Specify the variable to hold the contents# with open(output_path, 'w') as csvfile:#     # Initialize csv.writer#     csvwriter = csv.writer(csvfile, delimiter=',')#     # Write the Summary Headers#     csvwriter.writerow(["'''text"])#     csvwriter.writerow(["Election Results"])#     csvwriter.writerow(["-----------------"])    #     # Write the Total Votes#     csvwriter.writerow(["Total Votes: "+total_votes])#     csvwriter.writerow(["-----------------"])    #     # Write the Candidate Percentages#     csvwriter.writerow([+cand_1+ ":  " +cand1_percent+ " "+cand1_votes])#     csvwriter.writerow([+cand_2+ ":  " +cand2_percent+ " "+cand2_votes])#     csvwriter.writerow([+cand_3+ ":  " +cand3_percent+ " "+cand3_votes])#     csvwriter.writerow([+cand_4+ ":  " +cand4_percent+ " "+cand4_votes])    #     #Write Winner#     csvwriter.writerow(["-----------------"])    csvwriter.writerow(["WINNER:  "+winner])    csvwriter.writerow(["-----------------"])
-=======
-# Import Dependenciesimport osimport csv#Define the source dataelection_csv=os.path.join("..","Resources","election_data.csv")#Read the csv filewith open (election_csv, "r") as csvfile:    #split the file at commas    csvreader = csv.reader(csvfile, delimiter=',')    #Skip the header line    header = next(csvreader)    #test to ensure data present    print (header)        #Convert Voter ID to integer    voter_id=int(election_data[0])             # Calculate Votes    total_votes =0     cand_1= "Correy"    cand_2= "Khan"    cand_3="Li"    cand_4="O'Tooley"    cand1_votes=0    cand2_votes=0    cand3_votes=0    cand4_votes=0    for i in range(0,len(voter_id)):         total_votes=total_votes+1        if cand1==candidate[i]:            cand1_votes=cand1_votes +1          elif cand2==candidate[i]:            cand2_votes=cand2_votes +1        elif cand3==candidate[i]:            cand2_votes=cand3_votes +1        elif cand3==candidate[i]:            cand3_votes=cand3_votes +1        elif cand4==candidate[i]:            cand4_votes=cand4_votes +1                       #Calculate Percentages      cand1_percent=cand1_votes/total_votes      cand2_percent=cand1_votes/total_votes      cand3_percent=cand1_votes/total_votes      cand4_percent=cand1_votes/total_votes        if cand1_percent>cand2_percent and cand1_percent>cand3_percent and cand1_percent>cand4_percent        winner=cand_1    elif cand2_percent>cand1_percent and cand2_percent>cand3_percent and cand2_percent>cand4_percent        winner=cand_2    elif cand3_percent>cand1_percent and cand3_percent>cand2_percent and cand3_percent>cand4_percent        winner=cand_3    elif cand4_percent>cand1_percent and cand4_percent>cand2_percent and cand4_percent>cand3_percent        winner=cand_4 #Define Output pathoutput_path = os.path.join("..", "Analysis", "ElectionAnalysis.csv")# Open the file using "write" mode. Specify the variable to hold the contentswith open(output_path, 'w') as csvfile:    # Initialize csv.writer    csvwriter = csv.writer(csvfile, delimiter=',')    # Write the Summary Headers    csvwriter.writerow(["'''text"])    csvwriter.writerow(["Election Results"])    csvwriter.writerow(["-----------------"])        # Write the Total Votes    csvwriter.writerow(["Total Votes: "+total_votes])    csvwriter.writerow(["-----------------"])        # Write the Candidate Percentages    csvwriter.writerow([+cand_1+ ":  " +cand1_percent+ " "+cand1_votes])    csvwriter.writerow([+cand_2+ ":  " +cand2_percent+ " "+cand2_votes])    csvwriter.writerow([+cand_3+ ":  " +cand3_percent+ " "+cand3_votes])    csvwriter.writerow([+cand_4+ ":  " +cand4_percent+ " "+cand4_votes])        #Write Winner    csvwriter.writerow(["-----------------"])    csvwriter.writerow(["WINNER:  "+winner])    csvwriter.writerow(["-----------------"])
->>>>>>> a9dfc8efc4b81b9f4c617ec112250cc0a4745843
+# PyPoll Homework
+
+# Import Dependencies
+import os
+import csv
+
+
+#Define the source data
+election_csv=os.path.join("..","Resources","election_data.csv")
+
+# Total Vote Counter
+total_votes = 0
+
+# Candidate Options and Vote Counters
+candidate_list = []
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+
+
+#Read the csv file
+with open (election_csv, "r") as csvfile:
+    #split the file at commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Skip the header line
+    header = next(csvreader)
+    
+    for row in csvreader:
+        #print (row)
+
+        #Add to total vote count
+        total_votes = total_votes + 1
+        
+        # Get candidate name from row
+        candidate_name = row[2]
+        
+        # If the candidate is not on list
+        if candidate_name not in candidate_list:
+
+            # Add new candidate to candidate list
+            candidate_list.append(candidate_name)
+
+            # And begin tracking that candidate's voter count
+            candidate_votes[candidate_name] = 0
+
+        # Then add a vote to that candidate's count
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+
+
+#Define Output path
+output_path = os.path.join("..", "Analysis", "ElectionAnalysis.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w') as csvfile:
+
+    # Initialize csv.writer
+    csvwriter = csv.writer(csvfile, delimiter=',')    
+    
+    # Create Total Votes Summary
+    votes_summary = (
+     f'Election Results\n'
+        f'-------------------------\n'
+        f'Total Votes: {total_votes}\n'
+        f'-------------------------\n')
+
+    # Print the Total Votes Summary (to terminal)
+    print(votes_summary)  
+        
+    # Write the Total Votes Summary
+    csvwriter.writerow([votes_summary])
+    
+    
+    # Determine the winner by looping through the counts
+    for candidate in candidate_votes:
+
+        # Retrieve vote count and percentage
+        votes = candidate_votes.get(candidate)
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        # Determine winning vote count and candidate
+        if (votes > winning_count):
+            winning_count = votes
+            winning_candidate = candidate
+
+        # Create Candidate Summary
+        candidate_summary  = f'{candidate}: {vote_percentage:.3f}% ({votes})\n'
+
+        # Print the output (to terminal)
+        print(candidate_summary) 
+
+        # Write the Total Votes Summary
+        csvwriter.writerow([candidate_summary])
+
+
+    # Create Winner Summary
+    winner_summary = (
+        f'-------------------------\n'
+        f'Winner: {winning_candidate}\n'
+        f'-------------------------\n')
+
+    # Print the output (to terminal)
+    print(winner_summary)  
+
+    # Write the Total Votes Summary
+    csvwriter.writerow([winner_summary]) 
